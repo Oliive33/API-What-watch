dc0f34b39ccef95b374205e2751a13b9bda46fa5
olivier villevieille <oliviervillevieille@hotmail.fr>
2021-06-29 15:30:03 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,54 @@
+const form = document.getElementById("searchForm");
+const searchInput = document.getElementById("searchInput");
+const result = document.getElementById("result");
+
+let search = "";
+let movies = [];
+
+const fetchMovies = async () => {
+  movies = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=688aedd33853b9bc571e123def98ece2&query=${search}`
+  ).then((res) => res.json());
+  console.log(movies);
+};
+
+const moviesDisplay = async () => {
+  await fetchMovies();
+
+  movies.results.length = 12;
+
+  result.innerHTML = movies.results
+    .map(
+      (movie) =>
+        `
+        <li>
+        <h2>${movie.original_title}</h2>
+        <div class="card-content">
+          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"></img>
+          <div class="infos">
+            <p>${movie.overview}</p>
+            <p>Popularité : ${movie.popularity} ⭐</p>
+          </div>
+        </div>
+      </li>
+
+        `
+    )
+    .join("");
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  search = searchInput.value;
+  //   fetchMovies();
+  moviesDisplay();
+});
+//loader
+
+const loader = document.querySelector(".loader");
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    loader.className += " hidden";
+  }, 2500);
+});
